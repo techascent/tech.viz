8026e4db3430bb913ab71ddc9909b8d58cf7ddfb
Chris Nuernberger <chris@techascent.com>
2020-01-27 14:20:16 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject techascent/tech.viz "0.1-SNAPSHOT"
+  :description "Visualization library"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns tech.viz})
